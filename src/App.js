153e464a9cf1fb7877014d562778fbd04ae50288
Luna9804 <56940000+Luna9804@users.noreply.github.com>
2021-11-03 22:13:16 -0400

    set up

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Year: 2022 Highschool: P-TECH</h2>
     </div>
   );
 }
